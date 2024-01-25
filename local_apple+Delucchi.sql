1ab21984b6603c7bb28cb4fa93d07b9d9536cbe6
Gastón Alejo Delucchi <gdelucchi@me.com>
2024-01-25 09:13:41 -0300

    Corrección final
    Aplicando correcciones de la exposición

--- local_apple+Delucchi.sql
+++ local_apple+Delucchi.sql
@@ -60,8 +60,17 @@ id_producto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(30) NOT NULL,
 marca VARCHAR(20) NOT NULL,
 descripcion TEXT(200) NOT NULL,
-precio NUMERIC(10, 2) NOT NULL,
-stock INT NOT NULL
+precio NUMERIC(10, 2) NOT NULL
+);
+
+-- Crear tabla stock
+CREATE TABLE IF NOT EXISTS stock(
+id_stock INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id_producto INT NOT NULL,
+stock INT NOT NULL,
+id_sucursal INT NOT NULL,
+FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
+FOREIGN KEY (id_sucursal) REFERENCES sucursales(id_sucursal)
 );
 
 -- Crear tabla equipos
@@ -74,25 +83,36 @@ almacenamiento VARCHAR(10) NOT NULL,
 estado VARCHAR(10) NOT NULL,
 condicion VARCHAR(10) NOT NULL,
 color VARCHAR(20) NOT NULL,
-precio NUMERIC(6, 2) NOT NULL,
-fecha_venta DATE NOT NULL
+precio NUMERIC(6, 2) NOT NULL
+);
+
+-- Crear tabla vendedores
+CREATE TABLE IF NOT EXISTS vendedores(
+id_vendedor INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+fecha_comienzo DATE NOT NULL,
+id_empleado INT NOT NULL,
+id_sucursal INT NOT NULL,
+FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado),
+FOREIGN KEY (id_sucursal) REFERENCES sucursales(id_sucursal)
+);
+
+-- Crear tabla tecnicos
+CREATE TABLE IF NOT EXISTS tecnicos(
+id_tecnico INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+fecha_comienzo DATE NOT NULL,
+id_empleado INT NOT NULL,
+id_sucursal INT NOT NULL,
+FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado),
+FOREIGN KEY (id_sucursal) REFERENCES sucursales(id_sucursal)
 );
 
 -- Crear tabla servicios
 CREATE TABLE IF NOT EXISTS servicios(
 id_servicio INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(50) NOT NULL,
-precio NUMERIC(9, 2) NOT NULL
-);
-
--- Crear tabla vendedores
-CREATE TABLE IF NOT EXISTS vendedores(
-id_vendedor INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-fecha_comienzo_puesto DATE NOT NULL,
-id_empleado INT NOT NULL,
-id_sucursal INT NOT NULL,
-FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado),
-FOREIGN KEY (id_sucursal) REFERENCES sucursal(id_sucursal)
+precio NUMERIC(9, 2) NOT NULL,
+id_tecnico INT,
+FOREIGN KEY (id_tecnico) REFERENCES tecnicos(id_tecnico)
 );
 
 -- Crear tabla ventas de productos
@@ -127,16 +147,6 @@ FOREIGN KEY (id_equipo) REFERENCES equipos(id_equipo),
 FOREIGN KEY (id_sucursal) REFERENCES sucursales(id_sucursal)
 );
 
--- Crear tabla tecnicos
-CREATE TABLE IF NOT EXISTS tecnicos(
-id_tecnico INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-fecha_comienzo_puesto DATE NOT NULL,
-id_empleado INT NOT NULL,
-id_sucursal INT NOT NULL,
-FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado),
-FOREIGN KEY (id_sucursal) REFERENCES sucursal(id_sucursal)
-);
-
 
 
 -- Ver tabla clientes
@@ -154,14 +164,20 @@ SELECT * FROM empleados;
 -- Ver tabla productos
 SELECT * FROM productos;
 
+-- Ver tabla stock
+SELECT * FROM stock;
+
 -- Ver tabla equipos
 SELECT * FROM equipos;
 
--- Ver tabla servicios
-SELECT * FROM servicios;
-
 -- Ver tabla vendedores
 SELECT * FROM vendedores;
+
+-- Ver tabla tecnicos
+SELECT * FROM tecnicos;
+
+-- Ver tabla servicios
+SELECT * FROM servicios;
 
 -- Ver tabla ventas de productos
 SELECT * FROM venta_productos;
@@ -169,5 +185,7 @@ SELECT * FROM venta_productos;
 -- Ver tabla ventas de equipos
 SELECT * FROM venta_equipos;
 
--- Ver tabla tecnicos
-SELECT * FROM tecnicos;
+
+
+-- Eliminar base de datos
+DROP DATABASE apple_store_la_plata_bs_as;
